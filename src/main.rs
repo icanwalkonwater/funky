a2769a5b11fb3a7255888d5c22a400d1ff8e3943
Lucas Malandrino <lucas.malandrino@gmail.com>
2024-05-29 23:58:10 +0200

    feat: Adds clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, path::PathBuf};
+
+use clap::Parser;
+
+#[derive(Debug, clap::Parser)]
+struct CliOpts {
+    filename: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opts = CliOpts::parse();
+    println!("Parsing entry point at {}", opts.filename.display());
+    Ok(())
 }
